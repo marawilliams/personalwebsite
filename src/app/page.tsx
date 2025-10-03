211e238cd8b02a863b994387e68036deb5414f94
marawilliams <mara.williams005@gmail.com>
2025-10-03 17:16:00 -0400

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import LandingPage from "@/components/LandingPage";
 import AboutMe from "@/components/AboutMe";
-import LetsGo from "@/components/LetsGo";
 
 export default function Home() {
   return (
     <main>
       <LandingPage />
       <AboutMe />
-      <LetsGo />
     </main>
   );
 }
